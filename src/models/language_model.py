f6f1733367d728d0ec1906d28872543db6234be0
diego cordeiro <diegocordeiro121@gmail.com>
2023-12-07 16:05:42 -0300

    feat: implementando o metodo list_dicts na classe LanguageModel(req-4)

--- src/models/language_model.py
+++ src/models/language_model.py
@@ -9,7 +9,15 @@ class LanguageModel(AbstractModel):
         super().__init__(data)
 
     def to_dict(self):
-        return {
-            "name": self.data["name"],
-            "acronym": self.data["acronym"]
-        }
+        return {"name": self.data["name"], "acronym": self.data["acronym"]}
+
+    @classmethod
+    def list_dicts(cls, query={}):
+        final_data = list()
+        data = cls._collection.find(query)
+        for i in data:
+            final_data.append({
+                "name": i["name"],
+                "acronym": i["acronym"]
+            })
+        return final_data
