dd121178289e4567e80fb61169330005b05a2e05
diego cordeiro <diegocordeiro121@gmail.com>
2023-12-11 18:10:32 -0300

    tests: adicionando testes para o metodo list_as_json(req-8)

--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -1,7 +1,21 @@
-# import json
-# from src.models.history_model import HistoryModel
+import json
+from src.models.history_model import HistoryModel
 
 
 # Req. 7
-def test_request_history():
-    raise NotImplementedError
+def test_request_history(prepare_base):
+    history = json.loads(HistoryModel.list_as_json())
+    assert history == [
+        {
+            "_id": history[0]["_id"],
+            "text_to_translate": "Hello, I like videogame",
+            "translate_from": "en",
+            "translate_to": "pt",
+        },
+        {
+            "_id": history[1]["_id"],
+            "text_to_translate": "Do you love music?",
+            "translate_from": "en",
+            "translate_to": "pt",
+        },
+    ]
