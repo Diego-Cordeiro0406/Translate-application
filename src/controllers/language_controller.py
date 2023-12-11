d1240bb41fa6db38d1fca085d1720fc03c47cc31
diego cordeiro <diegocordeiro121@gmail.com>
2023-12-11 17:28:19 -0300

    feat: implementando a rota /reverse(req-7)

--- src/controllers/language_controller.py
+++ src/controllers/language_controller.py
@@ -8,13 +8,12 @@ language_controller = Blueprint("language_controller", __name__)
 
 @language_controller.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == 'POST':
+    if request.method == "POST":
         initial = request.form.get("text-to-translate")
         translate_from = request.form.get("translate-from")
         translate_to = request.form.get("translate-to")
         translate = GoogleTranslator(
-            source=translate_from,
-            target=translate_to
+            source=translate_from, target=translate_to
         ).translate(initial)
 
         translate_data = {
@@ -36,14 +35,21 @@ def index():
     return render_template("index.html", **translate_data)
 
 
-# @language_controller.route("/", methods=["POST"])
-# def index_post():
-#     ttt = (
-#         request.form.get("text-to-translate")
-#         if request.method == "POST"
-#         else ""
-#     )
-#     translate = GoogleTranslator(source="auto", target="pt").translate(
-#         ttt
-#     )
-#     return render_template("index.html", translated=translate)
+@language_controller.route("/reverse", methods=["POST"])
+def reverse_post():
+    initial_to_reverse = request.form.get("text-to-translate")
+    translate_from = request.form.get("translate-to")
+    translate_to = request.form.get("translate-from")
+
+    translate = GoogleTranslator(
+           source=translate_to, target=translate_from
+        ).translate(initial_to_reverse)
+
+    translate_data = {
+        "languages": LanguageModel.list_dicts(),
+        "text_to_translate": translate,
+        "translate_from": translate_from,
+        "translate_to": translate_to,
+        "translated": initial_to_reverse,
+        }
+    return render_template("index.html", **translate_data)
